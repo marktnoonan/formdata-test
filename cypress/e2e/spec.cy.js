d5f6a8e3004612fb0908c116828e0f25ab4e0164
Mark Noonan <mark@cypress.io>
2022-11-16 16:31:00 -0500

    set up example

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,15 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    const formData = new FormData()
+    const product = {};
+    formData.set('data', new Blob([JSON.stringify(product)], {type: 'application/json'}));
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:3000/test/post',
+      body: formData,
+    }).then(({ body }) => {
+      console.log(body) // -
+    })
   })
 })
